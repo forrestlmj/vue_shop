849ef015d299f07ec5b4cd473bbb2ed9b693eafb
yangchengkai <yangchengkai@yunzhangfang.com>
2020-05-13 09:21:33 +0800

    在mainjs中导入全局样式表，vue定义基本格式

--- src/componments/Login.vue
+++ src/componments/Login.vue
@@ -1,9 +1,15 @@
 <template>
-    <div>
+    <div class="login_container">
         登陆
     </div>
 </template>
 
+<script>
+export default {}
+</script>
 <style lang="less" scoped>
-
+.login_container {
+      background-color: #2b4b6b;
+      height: 100%;
+}
 </style>
